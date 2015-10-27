d5b39c3ab8023cbb238dce6924193c551781ad8e
Florian Hahn <flo@fhahn.com>
2015-10-27 10:01:43 +0100

    Move setup.py and MANIFEST.in to top level
    Now the package can be installed with pip from git

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #! /usr/bin/env python
 
 # System imports
-from distutils.core import *
-from distutils      import sysconfig
+from distutils.core import setup, Extension
+from distutils.command.build_py import build_py
 
 # Third-party modules - we depend on numpy for everything
 import numpy
@@ -15,25 +15,26 @@ except AttributeError:
 
 # fastlayout extension module
 _fastlayout = Extension("_fastlayout",
-                   ["fastlayout.i","fastlayout.c"],
+                   ["fastlayout/fastlayout.i","fastlayout/fastlayout.c"],
                    include_dirs = [numpy_include],
                    extra_compile_args=["-O3", "-fopenmp", "-msse3"],
                    extra_link_args=["-fopenmp"],
                    )
 
 # fastlayout setup
-setup(  name        = "fastlayout",
+dist = setup(  name        = "fastlayout",
         description = "fastlayout implements the Fruchterman-Reingold layout algorithm in a fast, parallel way",
         author      = "Roman Cattaneo",
         author_email = "romanc@ethz.ch",
         url         = "https://www.sg.ethz.ch/",
         version     = "0.3.0",
-        py_modules  = ['fastlayout'],
+        py_modules  = ['fastlayout.fastlayout'],
         ext_modules = [_fastlayout],
-        
+        install_requires=['numpy', ],
+
         # Choose your license
         license='MIT',
-        
+
         # See https://pypi.python.org/pypi?%3Aaction=list_classifiers
         classifiers=[
             # How mature is this project? Common values are
@@ -54,3 +55,10 @@ setup(  name        = "fastlayout",
             'Programming Language :: Python :: 2.7',
         ],
         )
+
+
+
+# Rerun the build_py to ensure that swig generated py files are also copied
+build_py = build_py(dist)
+build_py.ensure_finalized()
+build_py.run()
